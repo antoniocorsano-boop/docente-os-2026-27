dcaf60ce92b4d2f683f85c490c4c83c83ae3df64
Antonio Corsano <antonio.corsano@gmail.com>
2026-09-11 18:49:30 +0200

    brand: refine thread endpoint to approved reference

--- product/src/components/brand/brand-mark-geometry.ts
+++ product/src/components/brand/brand-mark-geometry.ts
@@ -2,7 +2,7 @@ export const DOCENTE_OS_MARK_GEOMETRY = {
   viewBox: '0 0 64 64',
   loopPath: 'M18 14H32.5C44.6 14 52 21.4 52 32.1C52 43.4 44.2 50 33 50C27.6 50 22.8 48.3 19 45.2',
   stemPath: 'M18 14V35.5',
-  threadPath: 'M13 49L27.4 34.6C29.2 32.8 31.3 31.8 33.8 31.8C36.6 31.8 39 32.9 41.2 34.9',
+  threadPath: 'M13 49L27.4 34.6C28.8 33.2 30.2 32.5 31.9 32.4',
   dot: { cx: 31.5, cy: 31.2, r: 5.1 },
   strokeWidth: 8,
 } as const
