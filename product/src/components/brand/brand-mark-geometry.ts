aa6f17ee7ac9b0a691c79c04dcff957401ede499
Antonio Corsano <antonio.corsano@gmail.com>
2026-09-11 18:46:34 +0200

    brand: freeze approved mark geometry

---
+++ product/src/components/brand/brand-mark-geometry.ts
@@ -0,0 +1,17 @@
+export const DOCENTE_OS_MARK_GEOMETRY = {
+  viewBox: '0 0 64 64',
+  loopPath: 'M18 14H32.5C44.6 14 52 21.4 52 32.1C52 43.4 44.2 50 33 50C27.6 50 22.8 48.3 19 45.2',
+  stemPath: 'M18 14V35.5',
+  threadPath: 'M13 49L27.4 34.6C29.2 32.8 31.3 31.8 33.8 31.8C36.6 31.8 39 32.9 41.2 34.9',
+  dot: { cx: 31.5, cy: 31.2, r: 5.1 },
+  strokeWidth: 8,
+} as const
+
+export const DOCENTE_OS_MARK_COLORS = {
+  blue: '#2F6DF6',
+  blueDeep: '#245AD4',
+  navy: '#0B2D5B',
+  teal: '#35BDD0',
+  white: '#FFFFFF',
+  inverseSoft: '#DCEBFF',
+} as const
